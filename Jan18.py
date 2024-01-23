71b5eae4a900f24ad48e45931280714435c4c70c
narimaansamani11 <156989524+narimaansamani11@users.noreply.github.com>
2024-01-23 10:00:51 +0400

    Add files via upload

---
+++ Jan18.py
@@ -0,0 +1,13 @@
+#How to write anything
+print("Hello World")
+
+#Creat a variable then print
+a = 5
+print(a)
+
+#How to use input function
+input("Enter your name: ")
+
+#How to use input and print at the smae time
+name = input("What's your name: ")
+print(name)
