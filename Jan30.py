71c3df86262925765c27ae0323fa6f76b3aa0044
narimaansamani11 <156989524+narimaansamani11@users.noreply.github.com>
2024-01-30 09:59:16 +0400

    Add files via upload

---
+++ Jan30.py
@@ -0,0 +1,29 @@
+
+#How to print doc strings
+def x():
+    """
+    This code is for nothing,
+    This code is acually for practice
+    """
+    a = b + c
+    return a
+
+b = 10
+c = 12
+
+print(x.__doc__)
+
+
+
+#Prctice fot int, float, and str
+a = int(3.6)
+b = float(3.88)
+c = str("12")
+
+print(a)
+print(b)
+print(c)
+
+
+
+#
